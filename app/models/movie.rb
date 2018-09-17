33c46306475fcacfd349e628383b54e2bca42747
Hooopo Wang <hoooopo@gmail.com>
2018-09-17 17:33:32 +0800

    cosine sim

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -28,9 +28,28 @@ class Movie < ActiveRecord::Base
     intersection.to_f / union.to_f
   end
 
-  def recommendation_movies(limit: 10)
+  def cosine_sim(other_movie)
+    other_user_ratings = other_movie.ratings.map { |r| [r.user_id, r.rating] }.to_h
+    user_ratings       = self.ratings.map { |r| [r.user_id, r.rating] }.to_h
+
+    # 有共同评价的用户
+    union_user_ids = other_user_ratings.keys & user_ratings.keys
+    return 0 if union_user_ids.count == 0
+
+    u = other_user_ratings.values_at(*union_user_ids)
+    v = user_ratings.values_at(*union_user_ids)
+
+    dot_product = u.zip(v).map { |a, b| a*b }.sum
+
+    magnitude_u = Math.sqrt(u.map { |x| x*x }.sum)
+    magnitude_v = Math.sqrt(v.map { |x| x*x }.sum)
+    
+    cosine_similarity = dot_product.to_f / (magnitude_v * magnitude_u)
+  end
+
+  def recommendation_movies(limit: 10, using: :jaccard_sim)
     movie_with_sim_scores = Movie.where("id != ?", self.id).map do |movie|
-      {movie: movie, sim_score: jaccard_sim(movie)}
+      {movie: movie, sim_score: public_send(using, movie) }
     end
     movie_with_sim_scores.sort_by { |h| 1 - h[:sim_score] }[0, limit]
   end
